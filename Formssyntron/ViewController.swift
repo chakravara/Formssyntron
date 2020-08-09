bde3cd1bd41fba3802c1c3a034f5335e86e2b1cc
wozz <wazabidev@gmail.com>
2020-08-09 20:37:08 +0700

    Initial Commit

---
+++ Formssyntron/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Formssyntron
+//
+//  Created by Xoxo on 9/8/2563 BE.
+//  Copyright © 2563 wooz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
